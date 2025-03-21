04d9f37082563704067cf72f100c98db92377044
TheLittleDevil645 <pinakrathod@gmail.com>
2025-03-21 19:36:40 +0530

    Add files via upload

---
+++ specialEmail.js
@@ -0,0 +1,7 @@
+// Define the special email, username, and password
+const specialEmail = "themakeofthisgameisdumb@gmail.com";
+const specialUsername = "The OG";
+const specialPassword = "shivamkanzaria";
+
+// Export the special email, username, and password so they can be used in other files
+export { specialEmail, specialUsername, specialPassword };
